ccb160a99435e3a1892f350309adc6b785f39b98
Isaac <isaac.w.salt@gmail.com>
2025-03-17 12:52:59 +0000

    Index cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://YOURNAME.site',
-    markdown: {
-        shikiConfig: {
-            themes: {
-                light: 'snazzy-light',
-            },
-        },
-    },
+  site: 'https://YOURNAME.site',
+
+  markdown: {
+      shikiConfig: {
+          themes: {
+              light: 'snazzy-light',
+          },
+      },
+  },
+
+  integrations: [tailwind()],
 });
